0d363f5ee560f840b72d15277228ff44d794a9bd
parkjihyo <wlgy5678@gmail.com>
2026-05-12 11:09:08 +0900

    0512 3교시

---
+++ hello-react-vite/src/ch14/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import DummyJson from './dummyJson/DummyJson';
+import Ex1 from './ex/Ex1';
+import Jsonplaceholder from './jsonplaceholder/Jsonplaceholder';
+
+const App = () => {
+  return (
+    <div>
+      <h1>ch14 API Test</h1>
+      <h2>dummyJson</h2>
+      <DummyJson></DummyJson>
+      <h2>Ex1, JSONPlaceholder 통신 확인 </h2>
+      <Ex1></Ex1>
+      <h2>Jsonplaceholder</h2>
+      <Jsonplaceholder></Jsonplaceholder>
+    </div>
+  );
+};
+
+export default App;
